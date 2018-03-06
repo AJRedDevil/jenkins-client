0435a07c0643fd5430925ac9c39f25c61f87b3ab
AJRedDevil <ajan.shresh@gmail.com>
2018-03-06 11:07:10 +0545

    add logging debugger

--- playground/src/index.js
+++ playground/src/index.js
@@ -1,3 +1,10 @@
 // load dot env
 require('dotenv').config();
+const debug = require('debug')('api');
+
+// our packages
 const api = require('./api');
+
+const NAME = 'Test Jenkins API';
+
+debug('booting %o', NAME);
